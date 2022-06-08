ecc004db9440520941852082a5c5e52c0bed7905
Sajjad124 <181370124@gift.edu.pk>
2022-06-08 17:37:49 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <>
+    <h1>Top 10 Table Tennis Players</h1>
+    <p>Players detail</p>
+    <ol>
+      <li>Ma Long</li>
+      <li>Timo Boll</li>
+      <li>Xu Xin</li>
+      <li>Fen Zhendong</li>
+      <li>Lin Yun-ju</li>
+      <li>Hugo Gaoyuan</li>
+    </ol>
+  </>,
+  document.getElementById("root")
+);
